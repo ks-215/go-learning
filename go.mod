36c1d80c385022f8b47250fc52f4e19701aec61a
ks-215 <k6e2g3@gmail.com>
2026-01-24 18:40:05 +0900

    step3

---
+++ go.mod
@@ -0,0 +1,5 @@
+module learning
+
+go 1.25.3
+
+require golang.org/x/tour v0.1.0 // indirect
--- step3.go
+++ step3.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
+	"golang.org/x/tour/pic"
 )
 
 func sample1() {
@@ -145,18 +148,90 @@ func printSlice2(s string, x []int) {
 
 func sample10() {
 	a := make([]int, 5)
-	printSlice2("a:", a)
+	printSlice2("a", a)
 
 	b := make([]int, 0, 5)
-	printSlice2("b:", b)
+	printSlice2("b", b)
 
 	c := b[:2]
-	printSlice2("c:", c)
+	printSlice2("c", c)
 
-	d := c[2:5]
-	printSlice2("d:", d)
+	d := b[2:5]
+	printSlice2("d", d)
+}
+
+func sample11() {
+	board := [][]string{
+		{"_", "_", "_"},
+		{"_", "_", "_"},
+		{"_", "_", "_"},
+	}
+
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+func printSlice3(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+func sample12() {
+	var s []int
+	printSlice3(s)
+
+	s = append(s, 0)
+	printSlice3(s)
+
+	s = append(s, 1)
+	printSlice3(s)
+
+	s = append(s, 2, 3, 4)
+	printSlice3(s)
+}
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func sample13() {
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+func sample14() {
+	pow := make([]int, 11)
+	for i := range pow {
+		pow[i] = 1 << uint(i) //ビットシフト演算
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
+
+func Pic(dx, dy int) [][]uint8 {
+	image := make([][]uint8, dy)
+
+	for y := 0; y < dy; y++ {
+		image[y] = make([]uint8, dx)
+
+		for x := 0; x < dx; x++ {
+			image[y][x] = uint8(x + y)
+		}
+	}
+
+	return image
+}
+
+func sample15() {
+	pic.Show(Pic)
 }
 
 func main() {
-	sample10()
+	sample15()
 }
