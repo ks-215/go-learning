36c9cf4b4968915ca6158c2c3b0ac7783369eb70
ks-215 <k6e2g3@gmail.com>
2026-01-25 23:36:15 +0900

    step3

--- step3.go
+++ step3.go
@@ -134,14 +134,6 @@ func sample8() {
 	printSlice(s) //[7]
 }
 
-func sample9() {
-	var s []int
-	printSlice(s)
-	if s == nil {
-		fmt.Println("nil!!")
-	}
-}
-
 func printSlice2(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
@@ -232,6 +224,23 @@ func sample15() {
 	pic.Show(Pic)
 }
 
+type Vertex2 struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex2{
+	"Bell Labs": Vertex2{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex2{
+		37.42202, -122.08408,
+	},
+}
+
+func sample16() {
+	fmt.Println(m)
+}
+
 func main() {
-	sample15()
+	sample16()
 }
